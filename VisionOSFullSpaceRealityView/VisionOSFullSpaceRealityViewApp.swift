8a4e935d753e68039573a73b6428ca25c7bd1e2c
tokufxug <tokufxug@gmail.com>
2023-12-28 11:44:09 +0900

    Initial Commit

---
+++ VisionOSFullSpaceRealityView/VisionOSFullSpaceRealityViewApp.swift
@@ -0,0 +1,21 @@
+//
+//  VisionOSFullSpaceRealityViewApp.swift
+//  VisionOSFullSpaceRealityView
+//
+//  Created by Sadao Tokuyama on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct VisionOSFullSpaceRealityViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+
+        ImmersiveSpace(id: "ImmersiveSpace") {
+            ImmersiveView()
+        }.immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
